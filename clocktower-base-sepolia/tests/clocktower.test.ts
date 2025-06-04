898db889d5d53dc265f04270afb4295232617654
Hugo Marx <hugo@clocktower.finance>
2025-06-04 14:26:28 -0500

    initial commit

---
+++ clocktower-base-sepolia/tests/clocktower.test.ts
@@ -0,0 +1,74 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { BigInt, Address, Bytes } from "@graphprotocol/graph-ts"
+import { CallerLog } from "../generated/schema"
+import { CallerLog as CallerLogEvent } from "../generated/Clocktower/Clocktower"
+import { handleCallerLog } from "../src/clocktower"
+import { createCallerLogEvent } from "./clocktower-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let timestamp = BigInt.fromI32(234)
+    let checkedDay = BigInt.fromI32(234)
+    let caller = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let isFinished = "boolean Not implemented"
+    let newCallerLogEvent = createCallerLogEvent(
+      timestamp,
+      checkedDay,
+      caller,
+      isFinished
+    )
+    handleCallerLog(newCallerLogEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("CallerLog created and stored", () => {
+    assert.entityCount("CallerLog", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "CallerLog",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "timestamp",
+      "234"
+    )
+    assert.fieldEquals(
+      "CallerLog",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "checkedDay",
+      "234"
+    )
+    assert.fieldEquals(
+      "CallerLog",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "caller",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "CallerLog",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "isFinished",
+      "boolean Not implemented"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
